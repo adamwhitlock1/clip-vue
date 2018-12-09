946717da5950465111ea035bcbc575bfe6185c75
Adam Whitlock <adam.whitlock627@gmail.com>
2018-12-09 01:52:28 -0700

    changed out styles added transition group animation

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,28 @@
 module.exports = {
   root: true,
-  parser: 'babel-eslint',
+  parser: "babel-eslint",
   parserOptions: {
-    sourceType: 'module'
+    sourceType: "module"
   },
   env: {
     browser: true,
     node: true
   },
-  extends: 'airbnb-base',
+  extends: "airbnb-base",
   globals: {
     __static: true
   },
-  plugins: [
-    'html'
-  ],
-  'rules': {
-    'global-require': 0,
-    'import/no-unresolved': 0,
-    'no-param-reassign': 0,
-    'no-shadow': 0,
-    'import/extensions': 0,
-    'import/newline-after-import': 0,
-    'no-multi-assign': 0,
+  plugins: ["html"],
+  rules: {
+    "no-console": "off",
+    "global-require": 0,
+    "import/no-unresolved": 0,
+    "no-param-reassign": 0,
+    "no-shadow": 0,
+    "import/extensions": 0,
+    "import/newline-after-import": 0,
+    "no-multi-assign": 0,
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+    "no-debugger": process.env.NODE_ENV === "production" ? 2 : 0
   }
-}
+};
--- src/renderer/components/MainPage.vue
+++ src/renderer/components/MainPage.vue
@@ -1,26 +1,49 @@
 <template>
   <div class="container">
-    <div
-      class="row mt-4 clip-item"
-      v-for="(clipItem, index) in allClip"
-      v-bind:key="'clip'+index"
-      v-clipboard:copy=clipItem.text
-      v-clipboard:success="onCopy"
+    <transition-group
+      name="clip-list"
+      tag="p"
     >
-      <div class="col-md-11">
-        <span
-          class="item-span"
-          v-html="clipItem.html"
+      <div
+        class="row mt-2 shadow clip-item align-self-center bg-white rounded"
+        v-for="(clipItem, index) in allClip"
+        v-bind:key="'clip'+index"
+      >
+        <div
+          class="col-md-10 align-self-center p-3"
+          v-clipboard:copy=clipItem.text
+          v-clipboard:success="onCopy"
         >
-          <v-icon name="beer" />
-        </span>
-      </div>
+          <div
+            class="item-span align-self-center"
+            v-html="clipItem.html"
+          >
 
-      <div class="col-md-1">
-        <button class="btn btn-info btn-block shadow">copy</button>
-      </div>
-    </div>
+          </div>
+        </div>
 
+        <div
+          class="col-md-1 align-self-center"
+          v-clipboard:copy=clipItem.text
+          v-clipboard:success="onCopy"
+        >
+          <button
+            class="btn btn-primary p-3 shadow"
+            title="copy item"
+          >
+            <v-icon name="copy" /></button>
+        </div>
+
+        <div class="col-md-1 align-self-center">
+          <button
+            class="btn btn-danger p-3 shadow"
+            title="delete item from list"
+            @click="deleteItem(index)"
+          >
+            <v-icon name="ban" /></button>
+        </div>
+      </div>
+    </transition-group>
   </div>
 </template>
 
@@ -28,6 +51,7 @@
 // import path from 'path';
 // import fs from 'fs';
 // import os from 'os';
+
 const { clipboard } = require("electron"); // eslint-disable-line
 
 function copiedInArray(needle, haystack) {
@@ -46,7 +70,11 @@ export default {
   data() {
     return {
       allClip: [
-        { html: '<span>Welcome to Vue Copy</span>', text: 'init text' },
+        {
+          html: '<span>Welcome to Clip Vue</span>',
+          text: 'init text',
+          starred: false,
+        },
       ],
       clipboardAvailFormats: clipboard.availableFormats(),
       currentClipHTML: '',
@@ -58,25 +86,20 @@ export default {
       this.currentClipHTML = clipboard.readHTML();
       this.currentClipText = clipboard.readText();
 
-      const isUndefined = typeof this.allClip[0].html;
-      if (isUndefined !== 'undefined') {
-        const lastClip = this.allClip[0].html;
-        if (this.currentClipHTML !== lastClip) {
-          if (copiedInArray(clipboard.readText(), this.allClip) === false) {
-            this.allClip.unshift({
-              html: this.currentClipHTML,
-              text: this.currentClipText,
-            });
-          }
-          if (this.allClip[this.allClip.length - 1].text === 'init text') {
-            this.allClip.pop();
-          }
+      const lastClip = this.allClip[0].html;
+      if (this.currentClipHTML !== lastClip && this.currentClipHTML !== '') {
+        if (copiedInArray(clipboard.readText(), this.allClip) === false) {
+          this.allClip.unshift({
+            html: this.currentClipHTML,
+            text: this.currentClipText,
+          });
         }
-      } else if (copiedInArray(clipboard.readText(), this.allClip) === false) {
-        this.allClip.unshift({
-          html: this.currentClipHTML,
-          text: this.currentClipText,
-        });
+        if (
+          this.allClip[this.allClip.length - 1].text === 'init text' ||
+          this.allClip[this.allClip.length - 1].text === 'All items cleared'
+        ) {
+          this.allClip.pop();
+        }
       }
     }, 1000);
   },
@@ -87,6 +110,28 @@ export default {
         position: 'bottom-right',
         duration: 2500,
       });
+    },
+    deleteItem(item) {
+      clipboard.clear();
+      if (item === 0 && this.allClip.length === 1) {
+        this.allClip.unshift({
+          html: '<span>All items cleared</span>',
+          text: 'All items cleared',
+        });
+        this.allClip.splice(1, 1);
+        this.$toasted.show('ALL Items Deleted', {
+          theme: 'outline',
+          position: 'bottom-right',
+          duration: 2500,
+        });
+      } else {
+        this.allClip.splice(item, 1);
+        this.$toasted.show('Item Deleted', {
+          theme: 'outline',
+          position: 'bottom-right',
+          duration: 2500,
+        });
+      }
     },
   },
   computed: {},
@@ -105,5 +150,9 @@ body {
 
 .item-span div {
   padding: 7px 10px;
+}
+
+.clip-list-move {
+  transition: transform 1s;
 }
 </style>
